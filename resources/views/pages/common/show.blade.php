f4d90c22534f52273ad0fef5b5d7bebad2d0fc65
achmadAndiDev <mas.achmadandi@gmail.com>
2025-01-12 15:59:13 +0700

    update, addjust common pages show

--- resources/views/pages/common/show.blade.php
+++ resources/views/pages/common/show.blade.php
@@ -3,6 +3,8 @@
     if (!empty($resource)) {
         $title = $title ?? Lang::get(Str::snake($resource) . '.title');
     }
+    // print_r($data); die;
+    $colTwo = true;
 @endphp
 
 @extends('layouts.main')
@@ -22,43 +24,7 @@
         </nav>
     </div>
 
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <div class="d-flex justify-content-between">
-
-                <h6 class="m-0 font-weight-bold text-primary">Detail {{ $title }}</h6>
-            
-                <a href="{{ route($resource.'.index') }}" class="btn btn-secondary btn-sm">
-                    <i class="fas fa-arrow-left"></i> Kembali
-                </a>
-            </div>
-        </div>
-        <div class="card-body">
-            <table class="table table-hover table-striped">
-                <tbody>
-                    @foreach ($data as $key => $value)
-                        <tr>
-                            <th class="text-capitalize text-secondary" style="width: 20%;">
-                                {{-- <i class="fas fa-info-circle"></i>  --}}
-                                {{ __($resource . '.' . $key) }}
-                            </th>
-                            <td>
-                                @if ($key === 'gender')
-                                    <span class="badge badge-pill badge-info">
-                                        {{ $value === 'L' ? 'Laki-laki' : ($value === 'P' ? 'Perempuan' : '-') }}
-                                    </span>
-                                @elseif (Str::contains($key, 'date'))
-                                    {{ $value ? \Carbon\Carbon::parse($value)->format('d-m-Y') : '-' }}
-                                @else
-                                    {{ $value ?? '-' }}
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
+    <x-card-detail :resource="$resource" :data="$data" />
     
 </div>
 @endsection
